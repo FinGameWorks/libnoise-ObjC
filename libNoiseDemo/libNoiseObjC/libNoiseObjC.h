0b151f73f5d01723267d9e0c5d6b19cdc864adcd
Justin Fincher <zhtsu47@me.com>
2016-08-30 22:25:16 +0800

    initial commit

---
+++ libNoiseDemo/libNoiseObjC/libNoiseObjC.h
@@ -0,0 +1,19 @@
+//
+//  libNoiseObjC.h
+//  libNoiseObjC
+//
+//  Created by Fincher Justin on 16/8/30.
+//  Copyright © 2016年 JustZht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libNoiseObjC.
+FOUNDATION_EXPORT double libNoiseObjCVersionNumber;
+
+//! Project version string for libNoiseObjC.
+FOUNDATION_EXPORT const unsigned char libNoiseObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libNoiseObjC/PublicHeader.h>
+
+
